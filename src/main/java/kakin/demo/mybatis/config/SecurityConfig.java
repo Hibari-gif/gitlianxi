615f6497e4fc3394f206182ea08cbfdc5de88ed5
wangyi <1322265311@qq.com>
2022-02-09 18:31:05 +0800

    modefy

--- src/main/java/kakin/demo/mybatis/config/SecurityConfig.java
+++ src/main/java/kakin/demo/mybatis/config/SecurityConfig.java
@@ -34,6 +34,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
                 .and()
                 .httpBasic()
                 .and().logout().logoutUrl("/logout");
+
+                
         http.csrf().disable();
       }
     
